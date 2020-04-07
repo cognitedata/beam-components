355631e2702a00f982ca5f071bc4115fb9526c63
Shanthosh Pushparajah <shanthosh.pushparajah@cognite.com>
2020-04-07 15:58:12 +0200

    refactor naming & script

--- src/scripts/sh/replicator/RunReplicateTs-Dataflow.sh
+++ src/scripts/sh/replicator/RunReplicateTs-Dataflow.sh
@@ -1,13 +1,13 @@
 # The CDF source config file (instance, key, host)
-cdfSourceConfigFile='gs://beam-component/config/config-project-config-prod.toml'
+cdfSourceConfigFile='gs://sa-sandbox-training/beam-component/config/config-project-config-prod.toml'
 
 # The CDF target config file (instance, key, host)
-cdfTargetConfigFile='gs://beam-component/config/config-project-config-test.toml'
+cdfTargetConfigFile='gs://sa-sandbox-training/beam-component/config/config-project-config-test.toml'
 
 # The job config file
-jobConfigFile='gs://beam-component/config/config-project-config-test.toml'
+jobConfigFile='gs://sa-sandbox-training/beam-component/config/config-project-config-test.toml'
 
-mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.replicate.ReplicateTs -D exec.args="--cdfSourceConfigFile=${cdfSourceConfigFile} --cdfTargetConfigFile=${cdfTargetConfigFile} --jobConfigFile=${jobConfigFile} --project=cognite-sa-sandbox --runner=DataFlowRunner --gcpTempLocation=gs://akso-dataflow-test/temp --stagingLocation=gs://beam-component/stage/replicate-ts --region=europe-west1 --experiments=shuffle_mode=service --numWorkers=4 --maxNumWorkers=4 --workerMachineType=n2-standard-2"
+mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.replicate.ReplicateTs -D exec.args="--cdfSourceConfigFile=${cdfSourceConfigFile} --cdfTargetConfigFile=${cdfTargetConfigFile} --jobConfigFile=${jobConfigFile} --project=cognite-sa-sandbox --runner=DataFlowRunner --gcpTempLocation=gs://sa-sandbox-training/beam-component/temp --stagingLocation=gs://sa-sandbox-training/beam-component/stage/replicate-ts --region=europe-west1 --experiments=shuffle_mode=service --numWorkers=4 --maxNumWorkers=4 --workerMachineType=n2-standard-2"
 
 # Debug
 # --workerLogLevelOverrides='{\""com.cognite.beam.io\"":\""DEBUG\""}'
