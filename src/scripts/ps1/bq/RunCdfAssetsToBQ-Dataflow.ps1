4dd06837901fdfef903f72fcac768d5867025fc2
Kjetil Halvorsen <kjetil.halvorsen@cognite.com>
2020-05-17 19:31:48 +0200

    Add more comments.

--- src/scripts/ps1/bq/RunCdfAssetsToBQ-Dataflow.ps1
+++ src/scripts/ps1/bq/RunCdfAssetsToBQ-Dataflow.ps1
@@ -1,13 +1,31 @@
 . $PSScriptRoot'\..\variables.ps1'
 . $PSScriptRoot'\bqVariables.ps1'
 # Default variable values are picked up from the above file.
-# You can override them with local variable definitions here.
-
-# The CDF config file (host, instance/project, api)
+# You can override them with local variable definitions in this file.
+#
+#-------------------------------------------------------------------
+# The CDF config file (host, api)
+#
+# In case you use a config file to communicate the api key (and api host)
+# then set the reference here.
+#
+# Please note that the defaul is to use GCP Secret Manager for communicating the api key.
+# If Secret Manager is unavailable (i.e. running on Flink in Azure), then use a
+# config file instead.
+#-------------------------------------------------------------------
 #$cdfConfigFile = 'gs://beam-component/config/project-config-test.toml'
 
-# The CDF config file (instance, key, host)
+#-------------------------------------------------------------------
+# The CDF api key and host.
+#
+# The api key is read from GCP Secret Manager. Add the Secret Manager reference
+# to read from.
+#
+# CDF api host must be set if you use a dedicated tenant. For example:
+# https://<myTenantHost>.cognitedata.com
+#-------------------------------------------------------------------
 #$cdfSecret = 'projectId.secretId'
+#$cdfHost = 'https://api.cognitedata.com'
 
 # The destination table in BQ
 $outputMainTable = $outputDataSet + '.cdf_asset'
