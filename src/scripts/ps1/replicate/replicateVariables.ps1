db6624345125811d4202f58f1888a4dd9565e616
Kjetil Halvorsen <kjetil.halvorsen@cognite.com>
2020-05-22 17:44:56 +0200

    Add replication variables

---
+++ src/scripts/ps1/replicate/replicateVariables.ps1
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------
+# The CDF api key and host.
+#
+# The api key is read from GCP Secret Manager. Add the Secret Manager reference
+# to read from.
+#
+# CDF api host must be set if you use a dedicated tenant. For example:
+# https://<myTenantHost>.cognitedata.com
+#-------------------------------------------------------------------
+$cdfInputSecret = 'projectId.secretId'
+$cdfOutputSecret = 'projectId.secretId'
+
+# The CDF host
+$cdfInputHost = 'https://api.cognitedata.com'
+$cdfOutputHost = 'https://api.cognitedata.com'
