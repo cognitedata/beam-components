efd6c172c359b9c85dfdde7954b972cc994f6cb1
Kjetil Halvorsen <kjetil.halvorsen@cognite.com>
2020-05-22 18:05:53 +0200

    Add variables

--- src/scripts/ps1/replicate/RunReplicateEvents-Dataflow.ps1
+++ src/scripts/ps1/replicate/RunReplicateEvents-Dataflow.ps1
@@ -1,4 +1,41 @@
-mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.replicate.ReplicateEvents -D exec.args="--cdfSourceConfigFile=gs://beam-component/config/config-project-config-prod.toml --cdfTargetConfigFile=gs:/beam-component/config/config-project-config-test.toml --jobConfigFile=gs://beam-component/config/replicate/job-config-events-replication.toml --project=cognite-sa-sandbox --runner=DataFlowRunner --gcpTempLocation=gs://beam-component/temp --stagingLocation=gs://beam-component/stage/replicate/replicate-events --region=europe-west1 --experiments=shuffle_mode=service --numWorkers=10 --maxNumWorkers=20 --experiments=enable_stackdriver_agent_metrics"
+. $PSScriptRoot'\..\variables.ps1'
+. $PSScriptRoot'\replicateVariables.ps1'
+# Default variable values are picked up from the above file.
+# You can override them with local variable definitions in this file.
+#
+#-------------------------------------------------------------------
+# The CDF config file (host, api)
+#
+# In case you use a config file to communicate the api key (and api host)
+# then set the reference here.
+#
+# Please note that the defaul is to use GCP Secret Manager for communicating the api key.
+# If Secret Manager is unavailable (i.e. running on Flink in Azure), then use a
+# config file instead.
+#-------------------------------------------------------------------
+#$cdfInputConfigFile = 'gs://beam-component/config/project-config-test.toml'
+#$cdfOutputConfigFile = 'gs://beam-component/config/project-config-test.toml'
+
+#-------------------------------------------------------------------
+# The CDF api key and host.
+#
+# The api key is read from GCP Secret Manager. Add the Secret Manager reference
+# to read from.
+#
+# CDF api host must be set if you use a dedicated tenant. For example:
+# https://<myTenantHost>.cognitedata.com
+#-------------------------------------------------------------------
+#$cdfInputSecret = 'projectId.secretId'
+#$cdfOutputSecret = 'projectId.secretId'
+#$cdfInputHost = 'https://api.cognitedata.com'
+#$cdfOutputHost = 'https://api.cognitedata.com'
+
+#---------------------------------------------------------------------
+# The job config file.
+#---------------------------------------------------------------------
+$jobConfigFile = 'gs://' + $gcpBucketPrefix + '-test/config/replicate/job-config-events-replication.toml'
+
+mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.replicate.ReplicateEvents -D exec.args="--cdfInputSecret=$cdfInputSecret --cdfInputHost=$cdfInputHost --cdfOutputSecret=$cdfOutputSecret --cdfOutputHost=$cdfOutputHost --jobConfigFile=$jobConfigFile --project=$gcpProject --runner=DataFlowRunner --gcpTempLocation=gs://$gcpBucketPrefix-test/temp --stagingLocation=gs://$gcpBucketPrefix-test/stage/replicate/replicate-events --region=europe-west1 --experiments=shuffle_mode=service --numWorkers=10 --maxNumWorkers=20 --experiments=enable_stackdriver_agent_metrics"
 
 # In order to add profiling:
 # --profilingAgentConfiguration='{\""APICurated\"" : true}'
--- src/scripts/ps1/replicate/RunReplicateRaw.ps1
+++ src/scripts/ps1/replicate/RunReplicateRaw.ps1
@@ -1,1 +1,38 @@
-mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.replicate.ReplicateRaw -D exec.args="--cdfInputConfigFile=gs://beam-component/config/config-project-config-prod.toml --cdfOutputConfigFile=gs://beam-component/config/config-project-config-test.toml --jobConfigFile=./src/config/job-config-raw-replication.toml"
+. $PSScriptRoot'\..\variables.ps1'
+. $PSScriptRoot'\replicateVariables.ps1'
+# Default variable values are picked up from the above file.
+# You can override them with local variable definitions in this file.
+#
+#-------------------------------------------------------------------
+# The CDF config file (host, api)
+#
+# In case you use a config file to communicate the api key (and api host)
+# then set the reference here.
+#
+# Please note that the defaul is to use GCP Secret Manager for communicating the api key.
+# If Secret Manager is unavailable (i.e. running on Flink in Azure), then use a
+# config file instead.
+#-------------------------------------------------------------------
+#$cdfInputConfigFile = 'gs://beam-component/config/project-config-test.toml'
+#$cdfOutputConfigFile = 'gs://beam-component/config/project-config-test.toml'
+
+#-------------------------------------------------------------------
+# The CDF api key and host.
+#
+# The api key is read from GCP Secret Manager. Add the Secret Manager reference
+# to read from.
+#
+# CDF api host must be set if you use a dedicated tenant. For example:
+# https://<myTenantHost>.cognitedata.com
+#-------------------------------------------------------------------
+#$cdfInputSecret = 'projectId.secretId'
+#$cdfOutputSecret = 'projectId.secretId'
+#$cdfInputHost = 'https://api.cognitedata.com'
+#$cdfOutputHost = 'https://api.cognitedata.com'
+
+#---------------------------------------------------------------------
+# The job config file.
+#---------------------------------------------------------------------
+$jobConfigFile = 'gs://' + $gcpBucketPrefix + '-test/config/replicate/job-config-raw-replication.toml'
+
+mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.replicate.ReplicateRaw -D exec.args="--cdfInputSecret=$cdfInputSecret --cdfInputHost=$cdfInputHost --cdfOutputSecret=$cdfOutputSecret --cdfOutputHost=$cdfOutputHost --jobConfigFile=$jobConfigFile"
