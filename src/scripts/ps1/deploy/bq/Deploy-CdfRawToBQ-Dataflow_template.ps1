34705fbce2ff8ecd8b8cf29618c14fcdb058402e
kjetilh33 <kjetil.halvorsen@cognite.com>
2020-08-02 19:21:22 +0200

    Add new machine type. Fix BQ relationship main class name.

--- src/scripts/ps1/deploy/bq/Deploy-CdfRawToBQ-Dataflow_template.ps1
+++ src/scripts/ps1/deploy/bq/Deploy-CdfRawToBQ-Dataflow_template.ps1
@@ -2,4 +2,4 @@
 . $PSScriptRoot'\..\base-config.ps1'
 $postfix = Get-Postfix
 
-mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.bq.CdfRawBQ -D exec.args="--project=$gcpProject --runner=DataFlowRunner --gcpTempLocation=gs://$gcpBucketPrefix$postfix/temp --stagingLocation=$gcpBucketPrefix$postfix/template-stage/bq/cdf-raw-bq --templateLocation=$gcpBucketPrefix$postfix/template/bq/cdf-raw-bq --region=europe-west1 --experiments=shuffle_mode=service --numWorkers=2 --maxNumWorkers=8 --experiments=enable_stackdriver_agent_metrics"
+mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.bq.CdfRawBQ -D exec.args="--project=$gcpProject --runner=DataFlowRunner --gcpTempLocation=gs://$gcpBucketPrefix$postfix/temp --stagingLocation=$gcpBucketPrefix$postfix/template-stage/bq/cdf-raw-bq --templateLocation=$gcpBucketPrefix$postfix/template/bq/cdf-raw-bq --region=europe-west1 --experiments=shuffle_mode=service --numWorkers=1 --maxNumWorkers=4 --experiments=enable_stackdriver_agent_metrics --workerMachineType=e2-standard-2"
--- src/scripts/ps1/deploy/bq/Deploy-CdfTsPointsToBQ-Dataflow_template.ps1
+++ src/scripts/ps1/deploy/bq/Deploy-CdfTsPointsToBQ-Dataflow_template.ps1
@@ -2,4 +2,4 @@
 . $PSScriptRoot'\..\base-config.ps1'
 $postfix = Get-Postfix
 
-mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.bq.CdfTsPointsBQ -D exec.args="--project=$gcpProject --runner=DataFlowRunner --gcpTempLocation=gs://$gcpBucketPrefix$postfix/temp --stagingLocation=$gcpBucketPrefix$postfix/template-stage/cdf-ts-points-bq --region=europe-west1 --templateLocation=$gcpBucketPrefix$postfix/template/bq/cdf-ts-points-bq --experiments=shuffle_mode=service --numWorkers=10 --maxNumWorkers=20 --experiments=enable_stackdriver_agent_metrics"
+mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.bq.CdfTsPointsBQ -D exec.args="--project=$gcpProject --runner=DataFlowRunner --gcpTempLocation=gs://$gcpBucketPrefix$postfix/temp --stagingLocation=$gcpBucketPrefix$postfix/template-stage/cdf-ts-points-bq --region=europe-west1 --templateLocation=$gcpBucketPrefix$postfix/template/bq/cdf-ts-points-bq --experiments=shuffle_mode=service --numWorkers=4 --maxNumWorkers=10 --experiments=enable_stackdriver_agent_metrics --workerMachineType=e2-standard-2"
