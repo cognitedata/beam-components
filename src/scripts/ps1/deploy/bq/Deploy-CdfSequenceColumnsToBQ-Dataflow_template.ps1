541ee3c06ba99b0d298a5cdc9662f805c399d9bc
Stig-Martin Liavåg <stig-martin.liavag@visma.com>
2021-02-15 11:44:48 +0100

    Deploy scripts

---
+++ src/scripts/ps1/deploy/bq/Deploy-CdfSequenceColumnsToBQ-Dataflow_template.ps1
@@ -0,0 +1,5 @@
+. $PSScriptRoot'\..\..\variables.ps1'
+. $PSScriptRoot'\..\base-config.ps1'
+$postfix = Get-Postfix
+
+mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.bq.CdfSequenceColumnsBQ -D exec.args="--project=$gcpProject --runner=DataFlowRunner --gcpTempLocation=$gcpBucketPrefix$postfix/temp --stagingLocation=$gcpBucketPrefix$postfix/template-stage/bq/cdf-sequence-columns-bq --templateLocation=$gcpBucketPrefix$postfix/template/bq/cdf-sequence-columns-bq --region=europe-west1 --experiments=shuffle_mode=service --numWorkers=1 --maxNumWorkers=5 --experiments=enable_stackdriver_agent_metrics --workerMachineType=e2-standard-2"
