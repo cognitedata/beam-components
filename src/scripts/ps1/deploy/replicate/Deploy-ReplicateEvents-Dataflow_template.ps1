377c32960a5e9fbae3cada22f2efdd6716798642
Kjetil Halvorsen <kjetil.halvorsen@cognite.com>
2021-04-06 09:52:17 +0200

    Reduce default max workers

--- src/scripts/ps1/deploy/replicate/Deploy-ReplicateEvents-Dataflow_template.ps1
+++ src/scripts/ps1/deploy/replicate/Deploy-ReplicateEvents-Dataflow_template.ps1
@@ -7,4 +7,4 @@ $postfix = ''
 #
 #-------------------------------------------------------------------
 
-mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.replicate.ReplicateEvents -D exec.args="--project=$gcpProject --runner=DataFlowRunner --gcpTempLocation=$gcpBucketPrefix$postfix/temp --stagingLocation=$gcpBucketPrefix$postfix/template-stage/replicate/replicate-events --templateLocation=$gcpBucketPrefix$postfix/template/replicate/replicate-events --region=europe-west1 --experiments=shuffle_mode=service --numWorkers=2 --maxNumWorkers=4 --experiments=enable_stackdriver_agent_metrics --workerMachineType=e2-standard-2"
+mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.replicate.ReplicateEvents -D exec.args="--project=$gcpProject --runner=DataFlowRunner --gcpTempLocation=$gcpBucketPrefix$postfix/temp --stagingLocation=$gcpBucketPrefix$postfix/template-stage/replicate/replicate-events --templateLocation=$gcpBucketPrefix$postfix/template/replicate/replicate-events --region=europe-west1 --experiments=shuffle_mode=service --numWorkers=1 --maxNumWorkers=2 --experiments=enable_stackdriver_agent_metrics --workerMachineType=e2-standard-2"
--- src/scripts/ps1/deploy/replicate/Deploy-ReplicateRaw-Dataflow_template.ps1
+++ src/scripts/ps1/deploy/replicate/Deploy-ReplicateRaw-Dataflow_template.ps1
@@ -2,4 +2,4 @@
 . $PSScriptRoot'\..\base-config.ps1'
 $postfix = Get-Postfix
 
-mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.replicate.ReplicateRaw -D exec.args="--project=$gcpProject --runner=DataFlowRunner --gcpTempLocation=$gcpBucketPrefix$postfix/temp --stagingLocation=$gcpBucketPrefix$postfix/template-stage/replicate/replicate-raw --region=europe-west1 --templateLocation=$gcpBucketPrefix$postfix/template/replicate/replicate-raw --experiments=shuffle_mode=service --maxNumWorkers=4 --experiments=enable_stackdriver_agent_metrics --workerMachineType=e2-standard-2"
+mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.replicate.ReplicateRaw -D exec.args="--project=$gcpProject --runner=DataFlowRunner --gcpTempLocation=$gcpBucketPrefix$postfix/temp --stagingLocation=$gcpBucketPrefix$postfix/template-stage/replicate/replicate-raw --region=europe-west1 --templateLocation=$gcpBucketPrefix$postfix/template/replicate/replicate-raw --experiments=shuffle_mode=service --maxNumWorkers=2 --experiments=enable_stackdriver_agent_metrics --workerMachineType=e2-standard-2"
