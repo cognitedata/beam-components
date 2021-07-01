85186d4f39784a943956dbd7a70a99eaa834e1f1
Aksel Stokseth <aksel.stokseth@gmail.com>
2021-07-01 17:19:25 +0200

    add: beam job for replicating files metadata only

---
+++ src/scripts/ps1/deploy/replicate/Deploy-ReplicateFilesMetadata-Dataflow_template.ps1
@@ -0,0 +1,9 @@
+. $PSScriptRoot'\..\..\variables.ps1'
+. $PSScriptRoot'\..\base-config.ps1'
+$postfix = Get-Postfix
+# Default variable values are picked up from the above files.
+# You can override them with local variable definitions in this file.
+#
+#-------------------------------------------------------------------
+
+mvn compile exec:java -D exec.mainClass=com.cognite.sa.beam.replicate.ReplicateFilesMetadata -D exec.args="--project=$gcpProject --runner=DataFlowRunner --gcpTempLocation=$gcpBucketPrefix$postfix/temp --stagingLocation=$gcpBucketPrefix$postfix/template-stage/replicate/replicate-files --templateLocation=$gcpBucketPrefix$postfix/template/replicate/replicate-files --region=europe-west1 --experiments=shuffle_mode=service --numWorkers=1 --maxNumWorkers=2 --experiments=enable_stackdriver_agent_metrics --workerMachineType=e2-standard-4"
